3b8fcefba268aedabfc80a42175036124475737e
jiho <jhkkim0114@gmail.com>
2022-10-04 21:30:26 +0900

    update

--- app/src/main/java/com/example/jhkim20220925/MainActivity.kt
+++ app/src/main/java/com/example/jhkim20220925/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.jhkim20220925
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        val tab = tabLayout.newTab()
+        tab.text = "탭4"
+
+        tabLayout.addTab(tab)
+
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            tabLayout.selectTab(tab)
+        }
+
+
     }
 }
